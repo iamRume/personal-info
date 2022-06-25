f1bd01aacb1fc67ba9632e0f964ee73a6636c567
Rume Akpede <oghenerumemrakpor@gmail.com>
2022-06-25 12:57:42 +0100

    final

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const myName = 'Rume Akpede'
 const myHeight = "17.2"
 const myCountry = "Nigeria"
-alert(`Hello,
-My name is$[name]
-My height is $[height] M
-And im from $[country].`)
+
+alert(`
+Rume Akpede
+17.2M
+Nigeria.`)
 
