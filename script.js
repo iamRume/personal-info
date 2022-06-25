120af522499982c6509403aa928551c3c89450bc
Rume Akpede <oghenerumemrakpor@gmail.com>
2022-06-25 12:00:49 +0100

    final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const myName = "Rume Akpede";
 const myHeight = "17.2M";
 const myCountry = "Nigeria";
 alert(` 
-My name is +{name}
-My height is +{height}
-I  am from +{country}`)
+My name is +{Name}
+My height is +{Height}
+I  am from +{Country}`)
 
